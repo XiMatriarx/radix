dbe3f160d6a9dfd3e969d9a69155c01b6aed5659
Xi Matriarx <xi@ximatriarx.io>
2024-02-10 14:53:18 +0200

    init

---
+++ bin/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+read -p "Project name: " project
+
+if [ -z "$project" ]
+then
+	echo "Project name is required."
+	exit
+fi
+
+git clone https://github.com/XiMatriarx/radix.git --depth 1 --branch 'v0.0.1'
+mv radix $project
+cd $project
+rm -Rf .git
+rm -Rf bin
